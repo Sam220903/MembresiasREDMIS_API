e574b613149ad49b10887b20c41f82c04901ddb1
Sam22G <samuelpg03@gmail.com>
2025-02-20 23:27:04 -0600

    Acceso a endpoint en index

--- backend/public/index.php
+++ backend/public/index.php
@@ -30,13 +30,49 @@ $lastIndex = count($parts) - 1;
 $route = $parts[$lastIndex - 1] ?? null;
 $id = $parts[$lastIndex] ?? null;
 
-// If the last element is not numeric, swap positions
-if (!is_numeric($id)) {
-    $route = $id;
-    $id = null;
+switch($route) {
+    case 'login':
+        // Lee los datos del cuerpo de la solicitud
+        $data = json_decode(file_get_contents('php://input'), true);
+        $email = $data['email'];
+        $password = $data['password'];
+        $auth_controller = new AuthController($conn);
+
+        
+        // Intenta iniciar sesión con las credenciales proporcionadas
+        $token = $auth_controller->login($email, $password);
+        if ($token) {
+            // Si el inicio de sesión es exitoso, devuelve el token en formato JSON
+            echo json_encode(['token' => $token]);
+        } else {
+            // Si las credenciales son inválidas, devuelve un mensaje de error y código 401
+            http_response_code(401);
+            echo json_encode(['message' => 'Credenciales Invalidas']);
+        }
+        break;
+
+    case 'logout':
+        // Lee los datos del cuerpo de la solicitud
+        $data = json_decode(file_get_contents('php://input'), true);
+        $token = $data['token'];
+        $auth_controller = new AuthController($conn);
+        
+        // Intenta cerrar sesión con el token proporcionado
+        if ($auth_controller->logout($token)) {
+            // Si el cierre de sesión es exitoso, devuelve un mensaje de confirmación
+            echo json_encode(['message' => 'Logged out']);
+        } else {
+            // Si el token es inválido, devuelve un mensaje de error y código 400
+            http_response_code(400);
+            echo json_encode(['message' => 'Invalid token']);
+        }
+        break;
+
+    default:
+        // Si la acción solicitada no es reconocida, devuelve un mensaje de error y código 404
+        http_response_code(404);
+        echo json_encode(['message' => 'Action not found']);
+        break;
+        
+        
 }
-
-echo "Route: $route, ID: $id";
-
-// Now $route will contain your endpoint and $id will contain the ID (if present)
-// regardless of how many folder levels you have
