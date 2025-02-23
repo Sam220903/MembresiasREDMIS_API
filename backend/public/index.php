994aeb1c891221b712544f22a3eb6d8a72a9d585
Sam22G <samuelpg03@gmail.com>
2025-02-22 23:46:11 -0600

    Roles y permisos (prueba)

--- backend/public/index.php
+++ backend/public/index.php
@@ -22,8 +22,18 @@ spl_autoload_register(function ($class) {
 include_once '../src/Config/header.php';
 include_once '../src/config/config.php';
 
+
+
+
 // Única conexión a la base de datos
 $database = new Database($connection["servername"], $connection["username"], $connection["password"], $connection["dbname"]);
+
+// Instancia de objetos para manejo de autorizaciones y roles
+$jwt = new JWT("1234567");
+$auth_middleware = new AuthMiddleware($jwt, $connection["login"]);
+$token_gateway = new TokenGateway($database);
+
+
 
 // Encuentra la ruta y el id en la URL
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -38,8 +48,16 @@ if (!is_numeric($id)) {
     $id = null;
 }
 
+// Manejo de autorización
+try {
+    $user_payload = $auth_middleware->handleRequest($route, $_SERVER["REQUEST_METHOD"], $tokenGateway, $id);
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(["error" => $e->getMessage()]);
+    exit();
+}
 
-// Este Switch se encarga de manejar las rutas de la API
+// Este switch se encarga de manejar las rutas de la API
 switch ($route){
     // Ruta para obtener todos los usuarios
     case "test":
