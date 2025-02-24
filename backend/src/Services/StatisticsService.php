3d766817ecb8b2378492a89dfe9dfa7f02e04e71
Sam22G <samuelpg03@gmail.com>
2025-02-23 20:50:33 -0600

    Endpoint de estadísticas terminado

--- backend/src/Services/StatisticsService.php
+++ backend/src/Services/StatisticsService.php
@@ -13,7 +13,13 @@ class StatisticsService{
     public function getStatistics(){
 
         // Al ser una consulta de selección, no es necesario hacer una preparación previa
-        $sql = "SELECT * FROM MR_Membresias;";
+        $sql = "SELECT CONCAT(m.nombre, ' ', m.apellidos) AS nombre_miembro, m.genero, m.email, u.nombre AS universidad, e.nombre AS estado, p.nombre AS pais, em.nombre AS estatus, tu.nombre AS tipo_usuario
+                FROM MR_Miembros m
+                JOIN MR_Universidades u ON m.MR_Universidades_id = u.id
+                JOIN MR_Estados e ON m.MR_Estados_id = e.id
+                JOIN MR_Paises p ON m.MR_Paises_id = p.id
+                JOIN MR_EstatusMiembros em ON m.MR_EstatusMiembros_id = em.id
+                JOIN MR_TiposUsuario tu ON m.MR_TiposUsuario_id = tu.id;";
         $result = $this->conn->query($sql);
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
