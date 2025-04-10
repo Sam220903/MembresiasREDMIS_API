fe39e39f880548b0b8e5bbeafbffb7e6e8ec7930
AntonioDev <gerardo.antonio01@upaep.edu.mx>
2025-04-10 01:21:30 -0600

    Solucion a SolicitudesMembresiasService

--- backend/src/Services/SolicitudesMembresiasService.php
+++ backend/src/Services/SolicitudesMembresiasService.php
@@ -7,13 +7,10 @@ class SolicitudesMembresiasService {
         $this->connection = $connection;
     }
 
-    public function getSolicitudesMembresias() : array {
-        $query = "SELECT s.id, CONCAT(mr.nombre, ' ', mr.apellidos) AS 'nombre', l.email, s.MR_Miembros_id,
-                    m.nombre AS 'membresia', s.estado, s.fecha_solicitud, s.fecha_respuesta, s.comentarios,
+    public function getSolicitudesMembresias() {
+        $query = "SELECT s.id, s.MR_Miembros_id, m.nombre AS 'membresia', s.estado, s.fecha_solicitud, s.fecha_respuesta, s.comentarios, 
                     IF( ISNULL(s.revisado_por), 'N/A', CONCAT(u.nombre, ' ', u.apellidos)) AS 'revisor'
                     FROM MR_SolicitudesMembresia s JOIN MR_Membresias m ON (MR_Membresias_id = m.id)
-                    JOIN MR_Miembros mr ON (s.MR_Miembros_id = mr.id)
-                    JOIN MR_Login l ON (mr.id = l.MR_Miembros_id)
                     LEFT JOIN MR_Miembros u ON (revisado_por = u.id)
                     ORDER BY s.id;";
         $stmt = $this->connection->prepare($query);
@@ -25,8 +22,7 @@ class SolicitudesMembresiasService {
         foreach ($results as $row) {
             $solicitud = [
                 'id' => $row['id'],
-                'nombre_usuario' => $row['nombre'],
-                'email_usuario' => $row['email'],
+                'usuario' => $this->getUserData($row['MR_Miembros_id']),
                 'membresia' => $row['membresia'],
                 'estado' => $row['estado'],
                 'fecha_solicitud' => $row['fecha_solicitud'],
@@ -39,6 +35,16 @@ class SolicitudesMembresiasService {
         }
 
         return $responseData;
+    }
+
+    private function getUserData($user_id){
+        $query = "SELECT CONCAT(m.nombre, ' ', m.apellidos) AS nombre, l.email
+                    FROM MR_Miembros m JOIN mr_db.MR_Login l on m.id = l.MR_Miembros_id
+                    WHERE m.id = :user_id;";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function getSolicitudporID($id) {
