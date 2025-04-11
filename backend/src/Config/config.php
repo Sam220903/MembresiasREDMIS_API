e3ca58b034f357ab623d9f9ad59d2b4954356019
rodrivmac1 <rodrigoriveramacias41@gmail.com>
2025-04-10 18:59:12 -0600

    Correccion

--- backend/src/Config/config.php
+++ backend/src/Config/config.php
@@ -9,17 +9,12 @@ $connection = [
 ];
 */
 
-$connection = [
-    "servername" => "localhost",
-    "username" => "mr_user",
-    "password" => "REDMIS",
-    "dbname" => "mr_db"
-];
+
 // La configuración fallback se usa cuando las variables de entorno no están disponibles
 // Configuración de la base de datos de PRODUCCIÓN
-// $connection = [
-//     "servername" => "lumacad.com.mx",
-//     "username" => "lumacadc_membresias_v2",
-//     "password" => "lumacadc_membresias_v2",
-//     "dbname" => "lumacadc_membresias"
-// ];
+ $connection = [
+     "servername" => "lumacad.com.mx",
+    "username" => "lumacadc_membresias_v2",
+    "password" => "lumacadc_membresias_v2",
+    "dbname" => "lumacadc_membresias"
+ ];
